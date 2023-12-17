a634f02d71b29e678e8c8826de66e71f81eb8d30
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2023-12-17 12:52:02 -0300

    Adding files we might need for the game and taking out the unused files

---
+++ src/conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    --t.window.title = "game name"
+    --t.window.icon = "assets/panda.png"
+end
--- src/main.lua
+++ src/main.lua
@@ -1,16 +1,8 @@
-require 'bolinha'
-
 function love.load()
-
 end
 
-function love.update ()
-    bolinha:updateSpeed()
-    bolinha:move()
+function love.update(dt)
 end
 
 function love.draw ()
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.arc('fill', bolinha.pos.x, bolinha.pos.y, 10, 0, 360)
-    love.graphics.arc('fill', 300, 300, bolinha.getSize(30), 0, 360)
 end
